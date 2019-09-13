c13a41f4841f23e7994b7c01989311c391712a4b
Matheus Nogueira <matheus.nogueira@redeagr.com>
2019-09-13 10:20:40 -0300

    Fix entities

--- src/main/kotlin/ag/orbia/minicurso/question/QuestionModel.kt
+++ src/main/kotlin/ag/orbia/minicurso/question/QuestionModel.kt
@@ -2,13 +2,11 @@ package ag.orbia.minicurso.question
 
 import ag.orbia.minicurso.user.UserModel
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.SequenceGenerator
+import javax.persistence.*
 import javax.validation.constraints.NotNull
 
 @Entity
+@Table(name = "question", schema = "forum")
 data class QuestionModel(
         @Id
         @SequenceGenerator(name = "QuestionGen", schema = "forum", allocationSize = 1, initialValue = 1, sequenceName = "question_sequence")
--- src/main/kotlin/ag/orbia/minicurso/user/UserModel.kt
+++ src/main/kotlin/ag/orbia/minicurso/user/UserModel.kt
@@ -1,13 +1,11 @@
 package ag.orbia.minicurso.user
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.SequenceGenerator
+import javax.persistence.*
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotNull
 
 @Entity
+@Table(name = "user", schema = "forum")
 data class UserModel(
         @Id
         @SequenceGenerator(name = "UserGen", schema = "authentication", allocationSize = 1, initialValue = 1, sequenceName = "user_sequence")
