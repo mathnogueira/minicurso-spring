6bee6141f30013c4fdd5de055615e50c3e9c6c74
Matheus Nogueira <matheus.nogueira@redeagr.com>
2019-09-13 09:26:40 -0300

    Project generated

---
+++ src/main/kotlin/ag/orbia/minicurso/MinicursoApplication.kt
@@ -0,0 +1,11 @@
+package ag.orbia.minicurso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinicursoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinicursoApplication>(*args)
+}
